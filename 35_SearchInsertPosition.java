50b03f57586c525ea1e3159fd7e673f84917f670
Monazir7866 <monazirhussain722@gmail.com>
2023-02-16 22:43:44 +0530

    leetCode

---
+++ 35_SearchInsertPosition.java
@@ -0,0 +1,41 @@
+// Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+
+// You must write an algorithm with O(log n) runtime complexity.
+
+ 
+
+// Example 1:
+
+// Input: nums = [1,3,5,6], target = 5
+// Output: 2
+// Example 2:
+
+// Input: nums = [1,3,5,6], target = 2
+// Output: 1
+// Example 3:
+
+// Input: nums = [1,3,5,6], target = 7
+// Output: 4
+
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int low=0;
+        int res=nums.length-1;
+      while(low<=res)
+      {
+          int mid=(low+res)/2;
+          if(target<nums[mid])
+          {
+              res=mid-1;
+          }
+          else if(target>nums[mid])
+          {
+              low=mid+1;
+          }
+          else {
+             return mid;
+          }
+      }
+        return low;
+    }
+}

c40455acdd490284cf001e1489481b53e6f11c07
Monazir7866 <monazirhussain722@gmail.com>
2023-02-16 22:45:40 +0530

    leetCode

--- 21_MergeTwoSortedList.java
+++ 21_MergeTwoSortedList.java
@@ -33,30 +33,36 @@
  * }
  */
 
-class Solution {
-    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+//  solution
+// class Solution {
+//     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
           
-        ListNode head = new ListNode(0);
+//         ListNode head = new ListNode(0);
         
-        if(list1 == null && list2 == null){
-           return null;
-        } 
-        if(list1 == null) {
-            return list2;
-        }
-        if(list2 == null){ 
-            return list1;
-        }
-        if(list1.val > list2.val) { 
-            head = list2;
-            list2 = list2.next;
-        }
-        else {
-            head = list1;
-            list1 = list1.next;
-        }
+//         if(list1 == null && list2 == null){
+//            return null;
+//         } 
+//         if(list1 == null) {
+//             return list2;
+//         }
+//         if(list2 == null){ 
+//             return list1;
+//         }
+//         if(list1.val > list2.val) { 
+//             head = list2;
+//             list2 = list2.next;
+//         }
+//         else {
+//             head = list1;
+//             list1 = list1.next;
+//         }
         
-        head.next = mergeTwoLists(list1, list2);
-        return head;
-    }
-}
+//         head.next = mergeTwoLists(list1, list2);
+//         return head;
+//     }
+
+//     @Override
+//     public String toString() {
+//         return "Solution []";
+//     }
+// }

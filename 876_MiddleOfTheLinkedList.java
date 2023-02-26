34e71446073d5c4895913eace6f49a217cb1ceb7
Monazir7866 <monazirhussain722@gmail.com>
2023-02-26 13:50:31 +0530

    LeetCode

---
+++ 876_MiddleOfTheLinkedList.java
@@ -0,0 +1,48 @@
+// Given the head of a singly linked list, return the middle node of the linked list.
+
+// If there are two middle nodes, return the second middle node.
+
+ 
+
+// Example 1:
+
+
+// Input: head = [1,2,3,4,5]
+// Output: [3,4,5]
+// Explanation: The middle node of the list is node 3.
+// Example 2:
+
+
+// Input: head = [1,2,3,4,5,6]
+// Output: [4,5,6]
+// Explanation: Since the list has two middle nodes with values 3 and 4, we return the second one.
+
+
+// Solution
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+// class Solution {
+//     public ListNode middleNode(ListNode head) {
+//         ListNode curr = head;
+//         int c = 0;
+
+//         while(curr != null){
+//             curr = curr.next;
+//             c++;
+//         }
+//         c = (int) Math.ceil(c / 2);
+//         for(int i = 0; i < c; i++){
+//             head = head.next;
+//         }
+//         return head;
+//     }
+// }

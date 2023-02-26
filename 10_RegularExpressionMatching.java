7783745851db6613a73d3af4df1f7ddb74dc8e06
Monazir7866 <monazirhussain722@gmail.com>
2023-02-26 13:41:12 +0530

    LeetCode

---
+++ 10_RegularExpressionMatching.java
@@ -0,0 +1,52 @@
+// Given an input string s and a pattern p, implement regular expression matching with support for '.' and '*' where:
+
+// '.' Matches any single character.​​​​
+// '*' Matches zero or more of the preceding element.
+// The matching should cover the entire input string (not partial).
+
+ 
+
+// Example 1:
+
+// Input: s = "aa", p = "a"
+// Output: false
+// Explanation: "a" does not match the entire string "aa".
+// Example 2:
+
+// Input: s = "aa", p = "a*"
+// Output: true
+// Explanation: '*' means zero or more of the preceding element, 'a'. Therefore, by repeating 'a' once, it becomes "aa".
+// Example 3:
+
+// Input: s = "ab", p = ".*"
+// Output: true
+// Explanation: ".*" means "zero or more (*) of any character (.)".
+
+class Solution {
+    public boolean isMatch(String s, String p) {
+        int m = s.length();
+        int n = p.length();
+        boolean[][] dp = new boolean[m+1][n+1];
+        dp[0][0] = true;
+        for (int i = 1; i <= n; i++) {
+            if (p.charAt(i-1) == '*') {
+                dp[0][i] = dp[0][i-2];
+            }
+        }
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (p.charAt(j-1) == '.' || p.charAt(j-1) == s.charAt(i-1)) {
+                    dp[i][j] = dp[i-1][j-1];
+                } else if (p.charAt(j-1) == '*') {
+                    dp[i][j] = dp[i][j-2];
+                    if (p.charAt(j-2) == '.' || p.charAt(j-2) == s.charAt(i-1)) {
+                        dp[i][j] = dp[i][j] || dp[i-1][j];
+                    }
+                } else {
+                    dp[i][j] = false;
+                }
+            }
+        }
+        return dp[m][n];
+    }
+}

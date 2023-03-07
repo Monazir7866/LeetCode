1941dc8daffe213542160774cfb8249f4c8e1a49
Monazir7866 <monazirhussain722@gmail.com>
2023-03-07 10:54:56 +0530

    LeetCode

--- 2187_MinimumTimeToCompleteTrips.java
+++ 2187_MinimumTimeToCompleteTrips.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 // You are given an array time where time[i] denotes the time taken by the ith bus to complete one trip.
 
 // Each bus can make multiple trips successively; that is, the next trip can start immediately after completing the current trip. Also, each bus operates independently; that is, the trips of one bus do not influence the trips of any other bus.

1c57455b1d444de74d438bf63cd2a88eba6ad728
Monazir7866 <monazirhussain722@gmail.com>
2023-03-14 14:06:07 +0530

    LeetCode

---
+++ 129_SumRootToLeafNumbers.java
@@ -0,0 +1,69 @@
+// You are given the root of a binary tree containing digits from 0 to 9 only.
+
+// Each root-to-leaf path in the tree represents a number.
+
+// For example, the root-to-leaf path 1 -> 2 -> 3 represents the number 123.
+// Return the total sum of all root-to-leaf numbers. Test cases are generated so that the answer will fit in a 32-bit integer.
+
+// A leaf node is a node with no children.
+
+ 
+
+// Example 1:
+
+
+// Input: root = [1,2,3]
+// Output: 25
+// Explanation:
+// The root-to-leaf path 1->2 represents the number 12.
+// The root-to-leaf path 1->3 represents the number 13.
+// Therefore, sum = 12 + 13 = 25.
+// Example 2:
+
+
+// Input: root = [4,9,0,5,1]
+// Output: 1026
+// Explanation:
+// The root-to-leaf path 4->9->5 represents the number 495.
+// The root-to-leaf path 4->9->1 represents the number 491.
+// The root-to-leaf path 4->0 represents the number 40.
+// Therefore, sum = 495 + 491 + 40 = 1026.
+
+
+
+// Solution
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+// class Solution {
+//     private int result = 0;
+
+//    public int sumNumbers(TreeNode root) {
+//        if(root == null) return -1;
+//        if(root.left == null && root.right == null) return root.val;
+//        sumNumbersHelper(root, "");
+//        return result;
+//    }
+
+//    private void sumNumbersHelper(TreeNode root, String number) {
+//        if(root == null) return;
+//        if(root.left == null && root.right == null)
+//            result += Integer.parseInt(number + root.val);
+
+//        sumNumbersHelper(root.left, number + root.val);
+//        sumNumbersHelper(root.right, number + root.val);
+//    }
+// }

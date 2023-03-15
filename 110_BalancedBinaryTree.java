e8921c9ac188dcb27964bb67376f6c41c9c8b97c
Monazir7866 <monazirhussain722@gmail.com>
2023-03-15 17:32:12 +0530

    LeetCode

--- 110_BalancedBinaryTree.java
+++ 110_BalancedBinaryTree.java
@@ -19,3 +19,44 @@
 // Input: root = []
 // Output: true
 
+
+
+// Solution
+
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+// class Solution {
+//     boolean isBal = true;
+//     public boolean isBalanced(TreeNode root) {
+//         height(root);
+//         return isBal;
+//     }
+//     public int height(TreeNode root){
+//         if(root == null){
+//             return 0;
+//         }
+//         int left = height(root.left);
+//         int right = height(root.right);
+        
+//         int diff = Math.abs(left - right);
+//         if(diff > 1){
+//             isBal = false;
+//         }
+//        int max = Math.max(left, right) + 1;
+//        return max;
+//     }
+// }

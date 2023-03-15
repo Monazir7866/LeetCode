45b43a4fa1b02fd1fc4a36e22e3284c7ee845d5f
Monazir7866 <monazirhussain722@gmail.com>
2023-03-15 17:17:00 +0530

    LeetCode

--- 958_CheckCompleteness.java
+++ 958_CheckCompleteness.java
@@ -17,4 +17,45 @@
 // Output: false
 // Explanation: The node with value 7 isn't as far left as possible.
 
+
+
 // Solution
+
+
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+// class Solution {
+//     public boolean isCompleteTree(TreeNode root) {
+//         Queue<TreeNode> que = new LinkedList<>();
+//         que.offer(root);
+//         boolean end = false;
+//         while(!que.isEmpty())
+//         {
+//             TreeNode cur = que.poll();
+//             if(cur == null)     
+//                 end = true;  
+//             else  // cur != null
+//             {
+//                 if(end == true) 
+//                     return false;
+//                 que.offer(cur.left);
+//                 que.offer(cur.right);
+//             }
+//         }
+//         return true; 
+//     }
+// }

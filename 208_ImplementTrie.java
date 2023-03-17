84596a6f3ea9ff201a1505f785c29bdf2bea8876
Monazir7866 <monazirhussain722@gmail.com>
2023-03-17 18:35:20 +0530

    LeetCode

---
+++ 208_ImplementTrie.java
@@ -0,0 +1,63 @@
+import java.util.HashSet;
+
+// A trie (pronounced as "try") or prefix tree is a tree data structure used to efficiently store and retrieve keys in a dataset of strings. There are various applications of this data structure, such as autocomplete and spellchecker.
+
+// Implement the Trie class:
+
+// Trie() Initializes the trie object.
+// void insert(String word) Inserts the string word into the trie.
+// boolean search(String word) Returns true if the string word is in the trie (i.e., was inserted before), and false otherwise.
+// boolean startsWith(String prefix) Returns true if there is a previously inserted string word that has the prefix prefix, and false otherwise.
+ 
+
+// Example 1:
+
+// Input
+// ["Trie", "insert", "search", "search", "startsWith", "insert", "search"]
+// [[], ["apple"], ["apple"], ["app"], ["app"], ["app"], ["app"]]
+// Output
+// [null, null, true, false, true, null, true]
+
+// Explanation
+// Trie trie = new Trie();
+// trie.insert("apple");
+// trie.search("apple");   // return True
+// trie.search("app");     // return False
+// trie.startsWith("app"); // return True
+// trie.insert("app");
+// trie.search("app");     // return True
+
+
+class Trie {
+
+    HashSet<String> hash;
+    public Trie() {
+        hash=new HashSet<>();
+    }
+    
+    public void insert(String word) {
+        hash.add(word);
+    }
+    
+    public boolean search(String word) {
+        if(hash.contains(word))
+            return true;
+        return false;
+    }
+    
+    public boolean startsWith(String prefix) {
+        for(String it:hash){
+            if(it.startsWith(prefix))
+                return true;
+        }
+        return false;
+    }
+}
+
+/**
+ * Your Trie object will be instantiated and called as such:
+ * Trie obj = new Trie();
+ * obj.insert(word);
+ * boolean param_2 = obj.search(word);
+ * boolean param_3 = obj.startsWith(prefix);
+ */

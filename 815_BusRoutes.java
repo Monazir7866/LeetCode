c71f6aaa534b24ff6d024af38145fc3e366b69a6
Monazir7866 <monazirhussain722@gmail.com>
2023-03-20 20:38:57 +0530

    LeetCode

--- 815_BusRoutes.java
+++ 815_BusRoutes.java
@@ -1,3 +1,8 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
+
 // You are given an array routes representing bus routes where routes[i] is a bus route that the ith bus repeats forever.
 
 // For example, if routes[0] = [1, 5, 7], this means that the 0th bus travels in the sequence 1 -> 5 -> 7 -> 1 -> 5 -> 7 -> 1 -> ... forever.
@@ -16,3 +21,59 @@
 
 // Input: routes = [[7,12],[4,5,15],[6],[15,19],[9,12,13]], source = 15, target = 12
 // Output: -1
+
+
+
+
+
+class Solution {
+    public static int numBusesToDestination(int[][] routes, int S, int T) {
+      int n = routes.length;
+      
+      HashMap<Integer, ArrayList<Integer>> map = new HashMap<>();
+      for(int i = 0 ; i < n ; i++){
+          for(int j = 0 ; j < routes[i].length ; j++){
+              int bustopno = routes[i][j];
+              ArrayList<Integer> busno = map.getOrDefault(bustopno, new ArrayList<>());
+              busno.add(i);
+              map.put(bustopno,busno);
+          }
+      }
+      
+      int level = 0;
+      LinkedList<Integer> queue = new LinkedList<>();
+      HashSet<Integer> busvis = new HashSet<>();
+      HashSet<Integer> bustopvis = new HashSet<>();
+      queue.addLast(S);
+      bustopvis.add(S);
+      while(queue.size() > 0){
+          int size = queue.size();
+          while(size-- > 0){
+              int rem = queue.remove();
+              
+              if(rem == T){
+                  return level;
+              }
+              
+              ArrayList<Integer> buses = map.get(rem);
+              for(int bus : buses){
+                  if(busvis.contains(bus) == true){
+                      continue;
+                  }
+                  int [] arr = routes[bus];
+                  for(int bustop : arr){
+                      if(bustopvis.contains(bustop) == true){
+                          continue;
+                      }
+                      queue.addLast(bustop);
+                  bustopvis.add(bustop);
+                  }
+                  busvis.add(bus);
+              }
+          }
+          level++;
+      }
+      return -1;
+      
+  }
+}

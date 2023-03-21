a3a0a872d405d2d2bcc555a2e0e00eae7fb79c21
Monazir7866 <monazirhussain722@gmail.com>
2023-03-21 11:42:14 +0530

    LeetCode

--- coinChange.java
+++ coinChange.java
@@ -1,21 +1,40 @@
-You are given an integer array coins representing coins of different denominations and an integer amount representing a total amount of money.
+import java.util.Arrays;
 
-Return the fewest number of coins that you need to make up that amount. If that amount of money cannot be made up by any combination of the coins, return -1.
+// You are given an integer array coins representing coins of different denominations and an integer amount representing a total amount of money.
 
-You may assume that you have an infinite number of each kind of coin.
+// Return the fewest number of coins that you need to make up that amount. If that amount of money cannot be made up by any combination of the coins, return -1.
+
+// You may assume that you have an infinite number of each kind of coin.
 
  
 
-Example 1:
+// Example 1:
 
-Input: coins = [1,2,5], amount = 11
-Output: 3
-Explanation: 11 = 5 + 5 + 1
-Example 2:
+// Input: coins = [1,2,5], amount = 11
+// Output: 3
+// Explanation: 11 = 5 + 5 + 1
+// Example 2:
 
-Input: coins = [2], amount = 3
-Output: -1
-Example 3:
+// Input: coins = [2], amount = 3
+// Output: -1
+// Example 3:
 
-Input: coins = [1], amount = 0
-Output: 0
+// Input: coins = [1], amount = 0
+// Output: 0
+
+
+class Solution {
+    public int coinChange(int[] coins, int amount) {
+        int[] dp = new int[amount + 1];
+        Arrays.fill(dp, amount + 1);
+        dp[0] = 0;
+        for (int i = 1; i <= amount; i++) {
+            for (int c : coins) {
+                if (c <= i) {
+                    dp[i] = Math.min(dp[i], 1 + dp[i - c]);
+                }
+            }
+        }
+        return dp[amount] != (amount + 1) ? dp[amount] : -1;
+    }
+}

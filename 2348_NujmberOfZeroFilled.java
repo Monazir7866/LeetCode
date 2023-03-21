8e13416ec958c3a303f0b8e7671aebbc3a7f41b9
Monazir7866 <monazirhussain722@gmail.com>
2023-03-21 10:54:59 +0530

    LeetCode

--- 2348_NujmberOfZeroFilled.java
+++ 2348_NujmberOfZeroFilled.java
@@ -26,3 +26,16 @@
 // Input: nums = [2,10,2019]
 // Output: 0
 // Explanation: There is no subarray filled with 0. Therefore, we return 0.
+
+
+class Solution {
+    public long zeroFilledSubarray(int[] nums) {
+        long res = 0;
+        for (int i = 0, j = 0; i < nums.length; ++i) {
+            if (nums[i] != 0)
+                j = i + 1;
+            res += i - j + 1;
+        }
+        return res;
+    }
+}

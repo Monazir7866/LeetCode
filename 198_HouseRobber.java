9a0c3aeea7d27efa687139554b26427ce8203fbf
Monazir7866 <monazirhussain722@gmail.com>
2023-03-21 10:58:53 +0530

    LeetCode

--- 198_HouseRobber.java
+++ 198_HouseRobber.java
@@ -16,3 +16,20 @@
 // Output: 12
 // Explanation: Rob house 1 (money = 2), rob house 3 (money = 9) and rob house 5 (money = 1).
 // Total amount you can rob = 2 + 9 + 1 = 12.
+
+
+
+class Solution {
+    public int rob(int[] nums) {
+        if(nums.length==0) return 0;
+        if(nums.length==1) return nums[0];
+        int first=nums[0];
+        int second=Math.max(first,nums[1]);
+        for(int i=2;i<nums.length;i++){
+            int temp=second;
+            second=Math.max(second,first+nums[i]);
+            first=temp;
+        }
+        return second;
+    }
+}

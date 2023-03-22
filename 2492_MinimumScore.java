95a8d2dae465077b1ce8ce599ce12754771455fa
Monazir7866 <monazirhussain722@gmail.com>
2023-03-22 11:03:50 +0530

    LeetCode

--- 2492_MinimumScore.java
+++ 2492_MinimumScore.java
@@ -1,3 +1,7 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+
 // You are given a positive integer n representing n cities numbered from 1 to n. You are also given a 2D array roads where roads[i] = [ai, bi, distancei] indicates that there is a bidirectional road between cities ai and bi with a distance equal to distancei. The cities graph is not necessarily connected.
 
 // The score of a path between two cities is defined as the minimum distance of a road in this path.
@@ -27,3 +31,37 @@
 
 
 
+class Pair{
+    int node;
+    int dist;
+    Pair(int node,int dist){
+        this.node=node;
+        this.dist=dist;
+    }
+}
+class Solution {
+    public int minScore(int n, int[][] roads) {
+        List<List<Pair>> adj=new ArrayList<>();
+        for(int i=0;i<n+1;i++)
+            adj.add(new ArrayList<>());
+        for(int i=0;i<roads.length;i++){
+            adj.get(roads[i][0]).add(new Pair(roads[i][1],roads[i][2]));
+            adj.get(roads[i][1]).add(new Pair(roads[i][0],roads[i][2]));
+        }
+        Queue<Pair> qu=new LinkedList<>();
+        boolean vis[]=new boolean[n+1];
+        qu.add(new Pair(1,Integer.MAX_VALUE));
+        int ans=Integer.MAX_VALUE;
+        while(!qu.isEmpty()){
+            Pair p=qu.poll();
+            vis[p.node]=true;
+            ans=Math.min(ans,p.dist);
+            for(Pair adjcomp:adj.get(p.node)){
+                if(!vis[adjcomp.node]){
+                    qu.add(adjcomp);
+                }
+            }
+        }
+        return ans;
+    }
+}

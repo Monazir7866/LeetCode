62d71cedeec6366ce22819a90cfbb1f36dc53497
Monazir7866 <monazirhussain722@gmail.com>
2023-03-22 11:13:02 +0530

    LeetCode

--- 416_PartitionEqual.java
+++ 416_PartitionEqual.java
@@ -14,3 +14,31 @@
 // Explanation: The array cannot be partitioned into equal sum subsets.
 
 
+class Solution {
+    Boolean mem[][];
+    public boolean canPartition(int[] nums) {
+        int sum = 0;
+        int n = nums.length;
+        
+        for(int i : nums) sum+=i;
+        
+        if(sum%2!=0) return false;
+        
+        sum /= 2;
+        
+        mem = new Boolean[n+1][sum+1];
+        
+        return subsetSum(nums,0,sum);
+    }
+    
+    boolean subsetSum(int[] nums, int pos, int sum){
+        if(sum==0) return true;
+        
+        else if(pos>=nums.length || sum<0) return false;
+        
+        if(mem[pos][sum]!=null) return mem[pos][sum];
+        
+        return mem[pos][sum] = subsetSum(nums,pos+1,sum-nums[pos]) ||
+                                subsetSum(nums,pos+1,sum);  
+    }
+}

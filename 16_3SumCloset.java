36060202a232670d3c2c2d1d5284cfe1fcd6f93c
Monazir7866 <monazirhussain722@gmail.com>
2023-03-23 09:53:54 +0530

    LeetCode

--- 16_3SumCloset.java
+++ 16_3SumCloset.java
@@ -16,3 +16,46 @@
 // Input: nums = [0,0,0], target = 1
 // Output: 0
 // Explanation: The sum that is closest to the target is 0. (0 + 0 + 0 = 0).
+
+
+
+class Solution
+{
+    public int threeSumClosest(int[] nums, int target) 
+    {
+        Arrays.sort(nums);
+        int sum=0;
+        int temp=0;
+        int max = Integer.MAX_VALUE;
+        int res=Integer.MAX_VALUE;
+        int out=0;
+        for (int i = 0; i < nums.length-1; i++)
+        {
+           int j=i+1;
+           int k= nums.length-1;
+           while(j<k)
+           {
+                sum = nums[i] + nums[j] + nums[k];
+               temp = Math.abs((sum - target));
+               if (temp < res)
+               {
+                   res = temp;
+                   out = sum; 
+               }
+               if (max<temp)
+               {
+                   j++;
+                   k= nums.length-1;
+                   max=Integer.MAX_VALUE;
+               }
+               else
+               {
+                   k--;
+                   max = temp;
+               }
+           }
+           if (out==target) break;  
+        }
+        return out;  
+    }
+}

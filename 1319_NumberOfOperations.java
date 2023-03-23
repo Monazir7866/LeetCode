8e0d7ca80e6891963eb2ee9100b12bb8fccec7a0
Monazir7866 <monazirhussain722@gmail.com>
2023-03-23 09:49:31 +0530

    LeetCode

--- 1319_NumberOfOperations.java
+++ 1319_NumberOfOperations.java
@@ -22,3 +22,26 @@
 // Input: n = 6, connections = [[0,1],[0,2],[0,3],[1,2]]
 // Output: -1
 // Explanation: There are not enough cables.
+
+
+class Solution {
+    public int makeConnected(int n, int[][] connections) {
+        if (connections.length < n - 1) return -1; 
+        int[] parent = new int[n];
+        for (int i = 0; i < n; i++) parent[i] = i;
+        int res = n;
+        for (int[] c : connections) {
+            int p1 = findParent(parent, c[0]);
+            int p2 = findParent(parent, c[1]);
+            if (p1 != p2) {
+                parent[p1] = p2; 
+                res--;
+            }
+        }
+        return res - 1;
+    }
+    private int findParent(int[] parent, int i) {
+        if (i == parent[i]) return i;
+        return parent[i] = findParent(parent, parent[i]); 
+    }
+}

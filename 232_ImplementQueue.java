430187fd79f30b50a0d6a960bb88432657632d47
Monazir7866 <monazirhussain722@gmail.com>
2023-03-25 21:55:45 +0530

    LeetCode

--- 232_ImplementQueue.java
+++ 232_ImplementQueue.java
@@ -32,30 +32,30 @@
 
 // Solution
 
-class MyQueue {
-    ArrayList st1 = new ArrayList();
-    public MyQueue() {
+// class MyQueue {
+//     ArrayList st1 = new ArrayList();
+//     public MyQueue() {
         
-    }
+//     }
     
-    public void push(int x) {
-        st1.add(x);
-    }
+//     public void push(int x) {
+//         st1.add(x);
+//     }
     
-    public int pop() {
-        return (int)st1.remove(0);
-    }
+//     public int pop() {
+//         return (int)st1.remove(0);
+//     }
     
-    public int peek() {
-        return (int)st1.get(0);
-    }
+//     public int peek() {
+//         return (int)st1.get(0);
+//     }
     
-    public boolean empty() {
-        if(st1.isEmpty())
-            return true;
-        return false;
-    }
-}
+//     public boolean empty() {
+//         if(st1.isEmpty())
+//             return true;
+//         return false;
+//     }
+// }
 
 /**
  * Your MyQueue object will be instantiated and called as such:

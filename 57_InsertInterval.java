2234dfa180d72f3eef52a75694e46231cf74df4a
Monazir7866 <monazirhussain722@gmail.com>
2023-03-26 17:40:14 +0530

    LeetCode

--- 57_InsertInterval.java
+++ 57_InsertInterval.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+
 // You are given an array of non-overlapping intervals intervals where intervals[i] = [starti, endi] represent the start and the end of the ith interval and intervals is sorted in ascending order by starti. You are also given an interval newInterval = [start, end] that represents the start and end of another interval.
 
 // Insert newInterval into intervals such that intervals is still sorted in ascending order by starti and intervals still does not have any overlapping intervals (merge overlapping intervals if necessary).
@@ -15,3 +18,31 @@
 // Input: intervals = [[1,2],[3,5],[6,7],[8,10],[12,16]], newInterval = [4,8]
 // Output: [[1,2],[3,10],[12,16]]
 // Explanation: Because the new interval [4,8] overlaps with [3,5],[6,7],[8,10].
+
+
+
+class Solution {
+    public int[][] insert(int[][] intervals, int[] newInterval) {
+        List<int[]> temp = new ArrayList<>();
+        int i = 0, n = intervals.length;
+        
+        while(i < n) {
+            if (intervals[i][0] > newInterval[1]) break;
+            else if (intervals[i][1] < newInterval[0])
+                temp.add(intervals[i]);
+            else {
+                newInterval[0] = Math.min(newInterval[0], intervals[i][0]);
+                newInterval[1] = Math.max(newInterval[1], intervals[i][1]);
+            }
+            i++;
+        }
+
+        temp.add(newInterval);
+        while (i < n) temp.add(intervals[i++]);
+
+        int[][] res = new int[temp.size()][2];
+        for (i = 0; i < temp.size(); i++) res[i] = temp.get(i);
+
+        return res;
+    }
+}

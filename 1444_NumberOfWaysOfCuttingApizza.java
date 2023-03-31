01ef540c57907bf0db22595eead7f9c5d641c09b
Monazir7866 <monazirhussain722@gmail.com>
2023-03-31 21:14:23 +0530

    LeetCode

--- 1444_NumberOfWaysOfCuttingApizza.java
+++ 1444_NumberOfWaysOfCuttingApizza.java
@@ -4,11 +4,7 @@
 
 // Return the number of ways of cutting the pizza such that each piece contains at least one apple. Since the answer can be a huge number, return this modulo 10^9 + 7.
 
- 
-
 // Example 1:
-
-
 
 // Input: pizza = ["A..","AAA","..."], k = 3
 // Output: 3 
@@ -21,3 +17,35 @@
 
 // Input: pizza = ["A..","A..","..."], k = 1
 // Output: 1
+
+class Solution {
+    public int ways(String[] pizza, int k) {
+        int m = pizza.length, n = pizza[0].length();
+        Integer[][][] dp = new Integer[k][m][n];
+        int[][] preSum = new int[m + 1][n + 1];
+        for (int r = m - 1; r >= 0; r--)
+            for (int c = n - 1; c >= 0; c--)
+                preSum[r][c] = preSum[r][c + 1] + preSum[r + 1][c] - preSum[r + 1][c + 1]
+                        + (pizza[r].charAt(c) == 'A' ? 1 : 0);
+        return dfs(m, n, k - 1, 0, 0, dp, preSum);
+    }
+
+    int dfs(int m, int n, int k, int r, int c, Integer[][][] dp, int[][] preSum) {
+        if (preSum[r][c] == 0)
+            return 0;
+        if (k == 0)
+            return 1;
+        if (dp[k][r][c] != null)
+            return dp[k][r][c];
+        int ans = 0;
+
+        for (int nr = r + 1; nr < m; nr++)
+            if (preSum[r][c] - preSum[nr][c] > 0)
+                ans = (ans + dfs(m, n, k - 1, nr, c, dp, preSum)) % 1_000_000_007;
+
+        for (int nc = c + 1; nc < n; nc++)
+            if (preSum[r][c] - preSum[r][nc] > 0)
+                ans = (ans + dfs(m, n, k - 1, r, nc, dp, preSum)) % 1_000_000_007;
+        return dp[k][r][c] = ans;
+    }
+}

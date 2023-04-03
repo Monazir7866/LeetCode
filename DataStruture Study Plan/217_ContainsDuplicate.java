3820d8e39e40afb0b07b75b1752125960862fb5b
Monazir7866 <monazirhussain722@gmail.com>
2023-04-03 22:13:10 +0530

    LeetCode

--- DataStruture Study Plan/217_ContainsDuplicate.java
+++ DataStruture Study Plan/217_ContainsDuplicate.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 // Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
 
  
@@ -14,3 +16,19 @@
 
 // Input: nums = [1,1,1,3,3,4,3,2,4,2]
 // Output: true
+
+
+
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Arrays.sort(nums);
+        boolean flag=false;
+        for(int i=0; i<nums.length-1; i++){
+            if(nums[i]==nums[i+1]){
+                flag=true;
+                break;
+            }
+        }
+        return flag;
+    }
+}

053dfc3baea2668e186baf16a79e6075fd415e0a
Monazir7866 <monazirhussain722@gmail.com>
2023-04-04 20:36:39 +0530

    LeetCode

--- 2405_OptimalPartitionOfString.java
+++ 2405_OptimalPartitionOfString.java
@@ -1,3 +1,5 @@
+import java.util.HashSet;
+
 // Given a string s, partition the string into one or more substrings such that the characters in each substring are unique. That is, no letter appears in a single substring more than once.
 
 // Return the minimum number of substrings in such a partition.
@@ -19,3 +21,21 @@
 // Output: 6
 // Explanation:
 // The only valid partition is ("s","s","s","s","s","s").
+
+
+class Solution {
+    public int partitionString(String s) {
+        int count=(s.isEmpty())? 0:1;
+        
+        s=s.toLowerCase();
+        HashSet<Character> letter =new HashSet<Character>();
+        for(int i=0;i<s.length();i++){
+            if(letter.contains(s.charAt(i))){
+                letter.clear();
+                count ++;
+            }
+            letter.add(s.charAt(i));
+        }
+        return count;
+    }
+}

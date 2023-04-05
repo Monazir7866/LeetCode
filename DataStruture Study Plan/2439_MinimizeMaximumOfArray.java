4241afda6198b73f8b9135daf91fe9466212c684
Monazir7866 <monazirhussain722@gmail.com>
2023-04-05 21:44:53 +0530

    LeetCode

--- DataStruture Study Plan/2439_MinimizeMaximumOfArray.java
+++ DataStruture Study Plan/2439_MinimizeMaximumOfArray.java
@@ -26,3 +26,15 @@
 // Output: 10
 // Explanation:
 // It is optimal to leave nums as is, and since 10 is the maximum value, we return 10.
+
+
+class Solution {
+    public int minimizeArrayValue(int[] nums) {
+        long sum = 0, res = 0;
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+            res = Math.max(res, (sum + i) / (i + 1));
+        }
+        return (int)res;
+    }
+}

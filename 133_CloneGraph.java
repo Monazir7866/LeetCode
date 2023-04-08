a241f16cff3259a942d3e819f0d3a20026050ea9
Monazir7866 <monazirhussain722@gmail.com>
2023-04-08 14:19:31 +0530

    LeetCode

--- 133_CloneGraph.java
+++ 133_CloneGraph.java
@@ -1,3 +1,7 @@
+import java.util.HashMap;
+
+import org.w3c.dom.Node;
+
 // Given a reference of a node in a connected undirected graph.
 
 // Return a deep copy (clone) of the graph.
@@ -41,3 +45,44 @@
 // Input: adjList = []
 // Output: []
 // Explanation: This an empty graph, it does not have any nodes.
+
+
+
+// Solution
+
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> neighbors;
+    public Node() {
+        val = 0;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val) {
+        val = _val;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val, ArrayList<Node> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+}
+*/
+
+// class Solution {
+//     HashMap<Node, Node> map = new HashMap<>();
+//     public Node cloneGraph(Node node) {
+//         if (node == null) return null;
+//         if (map.containsKey(node)) return map.get(node);
+
+//         Node newNode = new Node(node.val);
+//         map.put(node, newNode);
+        
+//         for (Node nb: node.neighbors) {
+//             newNode.neighbors.add(cloneGraph(nb));
+//         }
+
+//         return newNode;
+//     }
+// }

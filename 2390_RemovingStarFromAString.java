134aa609db5f17d0c85a956ce83b8a107d06c74c
Monazir7866 <monazirhussain722@gmail.com>
2023-04-11 17:47:48 +0530

    LeetCode

--- 2390_RemovingStarFromAString.java
+++ 2390_RemovingStarFromAString.java
@@ -26,3 +26,20 @@
 // Input: s = "erase*****"
 // Output: ""
 // Explanation: The entire string is removed, so we return an empty string.
+
+
+class Solution {
+    public String removeStars(String s) {
+        StringBuilder sb = new StringBuilder("");
+        for(int i=0;i<s.length();i++){
+            char ch = s.charAt(i);
+            if(ch!='*'){
+                sb.append(ch);
+            }
+            else{
+                sb.deleteCharAt(sb.length()-1);
+            }
+        }
+        return sb.toString();
+    }
+}

6e493885a718b73a018e8c9529a5958611ea2b54
Monazir7866 <monazirhussain722@gmail.com>
2023-04-25 23:00:54 +0530

    LeetCode

--- 2336_SmallestNumberInInfiniteSet.java
+++ 2336_SmallestNumberInInfiniteSet.java
@@ -1,28 +1,68 @@
-You have a set which contains all positive integers [1, 2, 3, 4, 5, ...].
+import java.util.PriorityQueue;
 
-Implement the SmallestInfiniteSet class:
+// You have a set which contains all positive integers [1, 2, 3, 4, 5, ...].
 
-SmallestInfiniteSet() Initializes the SmallestInfiniteSet object to contain all positive integers.
-int popSmallest() Removes and returns the smallest integer contained in the infinite set.
-void addBack(int num) Adds a positive integer num back into the infinite set, if it is not already in the infinite set.
+// Implement the SmallestInfiniteSet class:
+
+// SmallestInfiniteSet() Initializes the SmallestInfiniteSet object to contain all positive integers.
+// int popSmallest() Removes and returns the smallest integer contained in the infinite set.
+// void addBack(int num) Adds a positive integer num back into the infinite set, if it is not already in the infinite set.
  
 
-Example 1:
+// Example 1:
 
-Input
-["SmallestInfiniteSet", "addBack", "popSmallest", "popSmallest", "popSmallest", "addBack", "popSmallest", "popSmallest", "popSmallest"]
-[[], [2], [], [], [], [1], [], [], []]
-Output
-[null, null, 1, 2, 3, null, 1, 4, 5]
+// Input
+// ["SmallestInfiniteSet", "addBack", "popSmallest", "popSmallest", "popSmallest", "addBack", "popSmallest", "popSmallest", "popSmallest"]
+// [[], [2], [], [], [], [1], [], [], []]
+// Output
+// [null, null, 1, 2, 3, null, 1, 4, 5]
 
-Explanation
-SmallestInfiniteSet smallestInfiniteSet = new SmallestInfiniteSet();
-smallestInfiniteSet.addBack(2);    // 2 is already in the set, so no change is made.
-smallestInfiniteSet.popSmallest(); // return 1, since 1 is the smallest number, and remove it from the set.
-smallestInfiniteSet.popSmallest(); // return 2, and remove it from the set.
-smallestInfiniteSet.popSmallest(); // return 3, and remove it from the set.
-smallestInfiniteSet.addBack(1);    // 1 is added back to the set.
-smallestInfiniteSet.popSmallest(); // return 1, since 1 was added back to the set and
-                                   // is the smallest number, and remove it from the set.
-smallestInfiniteSet.popSmallest(); // return 4, and remove it from the set.
-smallestInfiniteSet.popSmallest(); // return 5, and remove it from the set.
+// Explanation
+// SmallestInfiniteSet smallestInfiniteSet = new SmallestInfiniteSet();
+// smallestInfiniteSet.addBack(2);    // 2 is already in the set, so no change is made.
+// smallestInfiniteSet.popSmallest(); // return 1, since 1 is the smallest number, and remove it from the set.
+// smallestInfiniteSet.popSmallest(); // return 2, and remove it from the set.
+// smallestInfiniteSet.popSmallest(); // return 3, and remove it from the set.
+// smallestInfiniteSet.addBack(1);    // 1 is added back to the set.
+// smallestInfiniteSet.popSmallest(); // return 1, since 1 was added back to the set and
+//                                    // is the smallest number, and remove it from the set.
+// smallestInfiniteSet.popSmallest(); // return 4, and remove it from the set.
+// smallestInfiniteSet.popSmallest(); // return 5, and remove it from the set.
+
+
+
+
+// Solution
+
+
+
+
+class SmallestInfiniteSet {
+    int SMALLEST;
+    PriorityQueue<Integer> minHeap;
+    public SmallestInfiniteSet() {
+        SMALLEST = 1;
+        minHeap = new PriorityQueue<>();
+    }
+    
+    public int popSmallest() {
+        if(minHeap.isEmpty()){
+            return SMALLEST++;
+        }
+        return minHeap.poll();
+    }
+    
+    public void addBack(int num) {
+        if(num >= SMALLEST || minHeap.contains(num)) {
+            return;
+        }
+        minHeap.add(num);
+    }
+}
+
+/**
+ * Your SmallestInfiniteSet object will be instantiated and called as such:
+ * SmallestInfiniteSet obj = new SmallestInfiniteSet();
+ * int param_1 = obj.popSmallest();
+ * obj.addBack(num);
+ */

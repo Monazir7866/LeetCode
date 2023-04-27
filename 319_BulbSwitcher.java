5835bbe279870e68478236288483a028642a9b7c
Monazir7866 <monazirhussain722@gmail.com>
2023-04-27 23:05:49 +0530

    LeetCode

--- 319_BulbSwitcher.java
+++ 319_BulbSwitcher.java
@@ -25,3 +25,19 @@
 // Input: n = 1
 // Output: 1
 
+class Solution {
+    public int bulbSwitch(int n) {
+        long low=0;
+        long high=n;
+        while(low<=high){
+            long mid=low+(high-low)/2;
+            if((mid*mid)==n)
+                return (int)mid;
+            else if((mid*mid)<n)
+                low=mid+1;
+            else
+                high=mid-1;
+        }
+        return (int)low-1;
+    }
+}

b3e3358458bde8a62b29e72eb927acf3fa4557f6
Monazir7866 <monazirhussain722@gmail.com>
2023-05-06 13:08:56 +0530

    Update 1498_NumberOfSub.java

--- 1498_NumberOfSub.java
+++ 1498_NumberOfSub.java
@@ -3,7 +3,6 @@
 // Return the number of non-empty subsequences of nums such that the sum of the minimum and maximum element on it is less or equal to target. Since the answer may be too large, return it modulo 109 + 7.
 
  
-
 // Example 1:
 
 // Input: nums = [3,5,6,7], target = 9
